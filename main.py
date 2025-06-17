2d6d4351c9d061df83f8e39133e9d08cbf03a768
ux-jlynn <ux@bubblynet.com>
2025-06-16 22:44:58 -0400

    feat: basic CLI to read different book paths

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_num_characters
 
 def get_book_text(file_path: str): 
@@ -11,8 +12,14 @@ def get_book_text(file_path: str):
 
 
 def main():
+    if len(sys.argv) <= 1:
+        print("Usage: python main.py <book_path>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+
     print("============ BOOKBOT ============")
-    contents = get_book_text("books/frankenstein.txt")
+    contents = get_book_text(book_path)
     
     print("--------- Word Count ----------")
     words = get_num_words(contents)
